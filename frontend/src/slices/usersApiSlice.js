c5e7e148cd1a1b8aeaab9812aaccd08ea3194007
Priyanshu Kumawat <priyanshukumawat434@gmail.com>
2023-12-06 21:48:27 +0530

    bug fix

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,46 +1,46 @@
 import { apiSlice } from "./apiSlice";
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (data) => ({
-                url: `${import.meta.env.USERS_URL}/users/login`,
+                url: `/api/users/login`,
                 method: 'POST',
                 body: data
             }),
         }),
         register: builder.mutation({
             query: (data) => ({
-                url: `${import.meta.env.USERS_URL}/users/`,
+                url: `/api/users/`,
                 method: 'POST',
                 body: data
             }),
         }),
         logout: builder.mutation({
             query: () => ({
-                url: `${import.meta.env.USERS_URL}/users/logout`,
+                url: `/api/users/logout`,
                 method: 'POST',
             })
         }),
         sendPrompt: builder.mutation({
             query: (data) => ({
-                url: `${import.meta.env.USERS_URL}/users/chat`,
+                url: `/api/users/chat`,
                 method: 'POST',
                 body: data
             }),
         }),
         getChatHistory: builder.mutation({
             query: () => ({
-                url: `${import.meta.env.USERS_URL}/users/history`,
+                url: `/api/users/history`,
                 method: 'GET',
             }),
         }),
         updateUser: builder.mutation({
             query: (data) => ({
-                url: `${import.meta.env.USERS_URL}/users/profile`,
+                url: `/api/users/profile`,
                 method: 'PUT',
                 body: data
             }),
